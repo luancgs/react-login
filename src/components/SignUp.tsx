8c055c246d2f6de2a7b52720197f6c1a5a1cfdfa
Luan <luan.carlos.3174@gmail.com>
2023-04-26 13:32:31 -0300

    chore: :necktie: updates sign up logic

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,17 +1,43 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 function SignUp() {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
-  const passwordConfirmationRef = useRef(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordConfirmationRef = useRef<HTMLInputElement>(null);
+
+  const { signUp } = useAuth();
+
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+
+    if (passwordRef.current?.value !== passwordConfirmationRef.current?.value) {
+      return setError('Passwords do not match.');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signUp(emailRef.current?.value as string, passwordRef.current?.value as string);
+    } catch {
+      setError('Failed to Sign Up');
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} />
@@ -24,13 +50,15 @@ function SignUp() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmationRef} />
             </Form.Group>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">Already have an account? Log In.</div>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>.
+      </div>
     </>
   );
 }
