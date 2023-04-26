cf71c7001eebff6ea05039dd699d5e887975c60e
Luan <luan.carlos.3174@gmail.com>
2023-04-26 13:34:14 -0300

    chore: :truck: adds app routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '../contexts/AuthContext';
 import SignUp from './SignUp';
+import LogIn from './LogIn';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
       <div className="w-100" style={{ maxWidth: '400px' }}>
-        <SignUp />
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<LogIn />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
       </div>
     </Container>
   );
